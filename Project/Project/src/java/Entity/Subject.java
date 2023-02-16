9c60a0a33f4bef97aec4a9057ba25eaa2ec69fa3
PIGB0SS <107063427+PIGB0SS@users.noreply.github.com>
2023-02-16 09:02:36 +0700

    Add files via upload

---
+++ Project/Project/src/java/Entity/Subject.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Entity;
+
+import java.util.*;
+
+/**
+ *
+ * @author doanv
+ */
+public class Subject {
+    String class_id;
+    String Id;
+    String Name;
+    String room;
+    Date date;
+    int slot;
+}
