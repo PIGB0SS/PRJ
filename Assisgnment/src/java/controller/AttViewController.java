09e4c5902bd78bce95b87f9e660aa514c3b244bf
PIGB0SS <107063427+PIGB0SS@users.noreply.github.com>
2023-03-23 09:09:44 +0700

    Add files via upload

---
+++ Assisgnment/src/java/controller/AttViewController.java
@@ -0,0 +1,104 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package controller;
+
+import dal.ViewDBContext;
+import java.io.IOException;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
+import model.Attendance;
+import model.Group;
+import model.Session;
+import model.Student;
+import model.TimeSlot;
+
+/**
+ *
+ * @author binhp
+ */
+public class AttViewController extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        int stdid = Integer.parseInt(request.getParameter("stdid"));
+        int id = (int) request.getSession().getAttribute("stdid");
+        if (id != stdid) {
+            response.getWriter().println("Access denied!");
+        } else {
+            ViewDBContext viewDB = new ViewDBContext();
+            Student student = viewDB.get(stdid);
+            ArrayList<Group> listGroup = viewDB.listGroup(stdid);
+            ArrayList<Attendance> listAttendance = viewDB.listAttendance(stdid);
+            ArrayList<Session> listSession = viewDB.listSession(stdid);
+            ArrayList<Attendance> atts = new ArrayList<>();
+            for (Session session : listSession) {
+                Attendance att = viewDB.getAttendance(stdid, session.getId());
+                atts.add(att);
+            }
+            request.setAttribute("attendances", atts);
+            ArrayList<TimeSlot> listTimeSlot = viewDB.listTimeSlot(stdid);
+            request.setAttribute("student", student);
+            request.setAttribute("groups", listGroup);
+            request.setAttribute("atts", listAttendance);
+            request.setAttribute("ses", listSession);
+            request.setAttribute("tes", listTimeSlot);
+            
+            request.getRequestDispatcher("../view/view.jsp").forward(request, response);
+        }
+
+    }
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ Assisgnment/src/java/controller/LoginController.java
@@ -0,0 +1,105 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package controller;
+
+import dal.AccountDBContext;
+import dal.LecturerDBContext;
+import dal.StudentDBContext;
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
+import model.Account;
+import model.Lecture;
+import model.Student;
+
+/**
+ *
+ * @author binhp
+ */
+public class LoginController extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        request.getRequestDispatcher("view/login.jsp").forward(request, response);
+
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        AccountDBContext db = new AccountDBContext();
+        Account account = db.get(username, password);
+
+//        response.getWriter().println("dis: " + displayname);
+//        if (s != null) {
+//            String stdname = s.getName();
+//            response.getWriter().println("stdname" + stdname);
+//        }
+//        if (l != null) {
+//            String lname = l.getName();
+//            response.getWriter().println("lname: " + lname);
+//        }
+        if (account != null) {
+            request.getSession().setAttribute("account", account);
+            LecturerDBContext lecDB = new LecturerDBContext();
+            Lecture l = lecDB.get(account.getDisplayname());
+            StudentDBContext stDB = new StudentDBContext();
+            Student s = stDB.get(account.getDisplayname());
+            if (s != null) {
+                request.getSession().setAttribute("stdid", s.getId());
+                response.sendRedirect("student/attview?stdid=" + s.getId());
+            } else if (l != null) {
+                request.getSession().setAttribute("lid", l.getId());
+                response.sendRedirect("lecture/timetable?lid=" + l.getId());
+            }
+        } else {
+            response.getWriter().println("Login failed!");
+        }
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
